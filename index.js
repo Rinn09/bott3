14830b4bbb2922f8a2893a6415bbe26935c73ddb
Rinn09 <154898426+Rinn09@users.noreply.github.com>
2025-02-16 11:31:19 +0700

    2

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const MusicQueue = require('./queue');
 const { YouTubeExtractor } = require('@discord-player/extractor');
 const path = require('node:path');
 const fs = require('node:fs');
-const { OpenAI } = require('openai');
 const mongoose = require('mongoose');
 const UserStat = require('./models/UserStat');
 const { token, testChannel, mongoosedbURI } = require('./config.json');
@@ -16,7 +15,6 @@ const disabledCommandsPath = path.join(__dirname, 'commands', 'utility', 'disabl
 const commandFolders = fs.readdirSync(foldersPath);
 const prefixModule = require('./prefix/prefix');
 const musicModule = require('./prefix/music');
-const chatBotChannel = ['1133046558271737926'];
 
 let userStats = {
   userMessages: {},
@@ -188,70 +186,6 @@ client.on('messageCreate', async message => {
     console.error('Error handling message:', error);
   }
 });
-
-/*
-client.on('messageCreate', async message => {
-  if (!chatBotChannel.includes(message.channelId) && !message.mentions.users.has(client.user.id)) return;
-  await message.channel.sendTyping();
-
-  const sendTypingInterval = setInterval(() => {
-    message.channel.sendTyping();
-  }, 3000);
-
-  let conversation = [];
-
-  conversation.push({
-    role: 'system',
-    content: 'Chat GPT is a friendly chatbot.'
-  });
-
-  let prevMessages = await message.channel.messages.fetch({ limit: 10 });
-  prevMessages.reverse();
-
-  prevMessages.forEach((msg) => {
-    if (msg.author.bot && msg.author.id !== client.user.id) return;
-    if (msg.content.startsWith(prefix)) return;
-
-    const username = msg.author.username.replace(/\s+/g, '_').replace(/[^\w\s]/gi, '');
-
-    if (msg.author.id === client.user.id) {
-      conversation.push({
-        role: 'assistant',
-        name: username,
-        content: msg.content,
-      });
-
-      return;
-    }
-
-    conversation.push({
-      role: 'user',
-      name: username,
-      content: msg.content,
-    });
-  });
-
-  const response = await openai.chat.completions.create({
-    model: 'text-davinci-003',
-    messages: conversation,
-  }).catch((error) => console.error('OpenAI error:/n', error));
-
-  clearInterval(sendTypingInterval);
-
-  if (!response) {
-    message.reply("Lỗi!");
-    return;
-  }
-
-  const responseMessage = response.choices[0].message.content;
-  const chunkSizeLimit = 2000;
-
-  for (let i = 0; i < responseMessage.length; i += chunkSizeLimit) {
-    const chunk = responseMessage.substring(i, i + chunkSizeLimit);
-    await message.reply(chunk);
-  }
-});
-*/
 
 async function updateExp(userId) {
   try {
